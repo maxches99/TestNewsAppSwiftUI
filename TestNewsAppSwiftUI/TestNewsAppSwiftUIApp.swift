1c480f6b53e12775d30b91b1424fc89f3199013d
Чесников Максим <maxim.chesnikov@chulakov.ru>
2021-02-16 11:56:45 +0300

    Initial Commit

---
+++ TestNewsAppSwiftUI/TestNewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewsAppSwiftUIApp.swift
+//  TestNewsAppSwiftUI
+//
+//  Created by Студия on 16.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestNewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
